2993a14b05b6d6cf51283e3fc72e0818bc6cd666
R20210406Takahashi <ulutor79@gmail.com>
2021-04-29 14:45:32 +0900

    JS学習一日目

---
+++ StudyforJS/try0001.js
@@ -0,0 +1,20 @@
+var a = "HEllow world";
+console.log(a);
+
+
+var tax = 1.08;
+
+var price =  console.log("商品の総額を入力して");
+
+var result = Math.round(price*tax);
+
+if(result>1000)
+{
+    console.log("ポイントカードはお持ちですか？");
+}
+else
+{
+    console.log("しけてんな～")
+}
+
+console.log("合計は"+result+"円です");
